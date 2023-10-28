e110fc4b69007afda56811888eaf451d535d9574
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-10-28 10:28:31 +0530

    train the model

--- recognition.py
+++ recognition.py
@@ -40,3 +40,9 @@ model.add(Dropout=0.5)
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+
+hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+print("The model has successfully trained.")
+
+model.save('mnist.h5')
+print("Saving the model as mnist.h5")
