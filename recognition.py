8318a3acd62375d40c86dcfcda25a009eb640154
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-10-28 18:32:32 +0530

    corrected the errors

--- recognition.py
+++ recognition.py
@@ -9,10 +9,12 @@ from keras import backend as K
 
 print(x_train.shape, y_train.shape)
 
+num_classes = 10
+
 #preprocess the data and adding a new dimesion to the matrix
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-input_Shape = (28, 28, 1)
+input_shape = (28, 28, 1)
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -29,14 +31,14 @@ batch_size = 128
 num_classes = 10
 epochs = 10
 
-mode = Sequential()
+model = Sequential()
 model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
-model.add(Dropout=0.5)
+model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
