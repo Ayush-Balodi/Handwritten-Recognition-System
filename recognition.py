53279c70e1f033a16a771b31e55af4dc551be773
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-10-28 10:23:47 +0530

    created the model

--- recognition.py
+++ recognition.py
@@ -24,3 +24,19 @@ x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train_samples')
 print(x_test.shape[0], 'test samples')
+
+batch_size = 128
+num_classes = 10
+epochs = 10
+
+mode = Sequential()
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout=0.5)
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
