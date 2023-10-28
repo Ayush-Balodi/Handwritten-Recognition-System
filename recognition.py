911f9900d594c5c2d442fefa7651eef1ed3f5ba3
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-10-28 10:34:50 +0530

    evaluate the model

--- recognition.py
+++ recognition.py
@@ -46,3 +46,8 @@ print("The model has successfully trained.")
 
 model.save('mnist.h5')
 print("Saving the model as mnist.h5")
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[0])
+print('Test accuracy:', score[1])
